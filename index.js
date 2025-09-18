c1923b106eecb7a3ecdf33cf7fe2bc1e3556ad4e
Alfred-Ndoro <alfred.ndoro@outlook.com>
2025-09-18 22:02:46 +0300

    continued functions

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// code your solution here
+//function saturdayFun(activity= "roller-skate") {
+  //console.log(`This Saturday, I want to ${activity}!`);
+//}
+//console.log(saturdayFun("go hiking"));
+//console.log(saturdayFun());
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+
+console.log(saturdayFun("go hiking")); // Output: This Saturday, I want to go hiking!
+console.log(saturdayFun());            // Output: This Saturday, I want to roller-skate!
+
+console.log(1 + 1);
+
+console.log("Razzle" + "dazzle");
+
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+
+console.log(mondayWork("work from home")); // Output: This Monday, I will work from home.
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a hard worker"));
+
+const defaultPrompt = wrapAdjective ();
+console.log(defaultPrompt());
+
